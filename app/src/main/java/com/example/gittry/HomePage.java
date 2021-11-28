d8f0353861df6395917aebb0d9d6ef2096151f95
andre <andreitoba80@gmail.com>
2021-11-29 00:00:42 +0200

    Am modificat signup in signupButton

--- app/src/main/java/com/example/gittry/HomePage.java
+++ app/src/main/java/com/example/gittry/HomePage.java
@@ -2,27 +2,24 @@ package com.example.gittry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
-Button singup;
+Button singupButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        singup=(Button) findViewById(R.id.SingUp);
+        singupButton =(Button) findViewById(R.id.SingUp);
 
-        singup.setOnClickListener(new View.OnClickListener() {
+        singupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomePage.this,Registration.class));
             }
         });
     }
-
 }
