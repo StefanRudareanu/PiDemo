84e506086f38c688cb9275c262c3fb25824f0839
andre <andreitoba80@gmail.com>
2021-11-29 01:47:46 +0200

    Register Activity XML Desgin - Done

--- app/src/main/java/com/example/gittry/Registration.java
+++ app/src/main/java/com/example/gittry/Registration.java
@@ -13,7 +13,7 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        BACK=(Button) findViewById(R.id.button5);
+        BACK=(Button) findViewById(R.id.registerButtonBack);
         BACK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
