26b282c3753c20e9421ab8d68c20a3e39c8161c3
StefanRudareanu <95167214+StefanRudareanu@users.noreply.github.com>
2021-11-28 13:50:27 -0800

    SSDA

--- app/src/main/java/com/example/gittry/HomePage.java
+++ app/src/main/java/com/example/gittry/HomePage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ Button singup;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         singup=(Button) findViewById(R.id.SingUp);
+
         singup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,13 +24,5 @@ Button singup;
             }
         });
     }
-//    public void iniateSwitch(Activity A,Activity B,Button b){
-//        b.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(A.this,B.class));
-//            }
-//        });
-//
-//    }
+
 }
--- app/src/main/java/com/example/gittry/Registration.java
+++ app/src/main/java/com/example/gittry/Registration.java
@@ -2,13 +2,23 @@ package com.example.gittry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
-
+    Button BACK;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        BACK=(Button) findViewById(R.id.button5);
+        BACK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registration.this,HomePage.class));
+            }
+        });
     }
 }
