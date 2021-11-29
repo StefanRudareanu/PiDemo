85d54db09ef0a32d5d1978df3f063288a0eb00e0
StefanRudareanu <95167214+StefanRudareanu@users.noreply.github.com>
2021-11-29 11:52:40 -0800

    SSDA

--- app/src/main/java/com/example/gittry/MainAplicationPage.java
+++ app/src/main/java/com/example/gittry/MainAplicationPage.java
@@ -2,36 +2,31 @@ package com.example.gittry;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.widget.TableLayout;
 
-import com.example.gittry.FragmentsForMainAplicationPage.MainAplicationPageFrameAdaptop;
+import com.example.gittry.FragmentsForMainAplicationPage.FragmentAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainAplicationPage extends AppCompatActivity {
-    TabLayout tableLayout;
+    TabLayout tablelayout;
     ViewPager2 pager2;
-    MainAplicationPageFrameAdaptop adapter;
+    FragmentAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_aplication_page);
-        tableLayout=findViewById(R.id.Table);
-        pager2=findViewById(R.id.ViepagerAM);
-
-        FragmentManager fm= getSupportFragmentManager();
-        adapter=new MainAplicationPageFrameAdaptop(fm,getLifecycle());
+        pager2=findViewById(R.id.pager2);
+        tablelayout=findViewById(R.id.tab_layout);
+        FragmentManager fm=getSupportFragmentManager();
+        adapter=new FragmentAdapter(fm,getLifecycle());
         pager2.setAdapter(adapter);
+        tablelayout.addTab(tablelayout.newTab().setText("Home"));
+        tablelayout.addTab(tablelayout.newTab().setText("Task"));
+        tablelayout.addTab(tablelayout.newTab().setText("Progress"));
 
-        tableLayout.addTab(tableLayout.newTab().setText("Home"));
-        tableLayout.addTab(tableLayout.newTab().setText("Tasks"));
-        tableLayout.addTab(tableLayout.newTab().setText("Progress"));
-
-        tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        tablelayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 pager2.setCurrentItem(tab.getPosition());
@@ -49,9 +44,9 @@ public class MainAplicationPage extends AppCompatActivity {
         });
         pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-               tableLayout.selectTab(tableLayout.getTabAt(position));
+            public void onPageSelected(int position) {
+                tablelayout.selectTab(tablelayout.getTabAt(position));
             }
         });
-    }
+            }
 }
